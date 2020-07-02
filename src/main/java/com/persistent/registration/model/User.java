0d6e5e2b7162d841f7abe55e28523b857083139d
Sandhya <sandhya27sb@gmail.com>
2020-07-02 23:06:27 +0530

    Error removed from User.java cls

--- src/main/java/com/persistent/registration/model/User.java
+++ src/main/java/com/persistent/registration/model/User.java
@@ -13,7 +13,7 @@ public class User {
 	@GeneratedValue
 	private int id;
 	
-	private  emailId;//error added
+	private  String emailId;
 	private String username;
 	private String password;
 	public int getId() {
